1850b3ceadda1393340b495533146aec4d03d5ee
Michal Bojanowski <michal2992@gmail.com>
2017-03-01 18:47:05 +0100

    Dane o dzielnicach Warszawy

---
+++ R/Warszawa.R
@@ -0,0 +1,28 @@
+#' Dzielnice Warszawy
+#'
+#' Dane o 18-tu dzielnicach Warszawy z Banku Danych Lokalnych.
+#'
+#' @docType data
+#' @name Warszawa
+#' @encoding UTF-8
+#' 
+#' @format Ramka danych z następującymi kolumnami:
+#' \describe{
+#' \item{Kod}{Kod TERYT}
+#' \item{Dzielnica}{Nazwa dzielnicy}
+#' \item{Strona_wisly}{Strona Wisły: lewa/prawa}
+#' \item{ekologiczne}{Teren: użytki ekologiczne [ha]}
+#' \item{inne}{Teren: inne użytki [ha]}
+#' \item{las}{Teren: zalesiony lub zakrzewiony [ha]}
+#' \item{nieużytki}{Teren: nieużytki [ha]}
+#' \item{rolne}{Teren: rolniczy [ha]}
+#' \item{woda}{Teren: woda (rzeki, stawy etc.) [ha]}
+#' \item{zabudowane}{Teren: zabudowany [ha]}
+#' \item{kobiety}{Ludność: liczba kobiet}
+#' \item{mężczyźni}{LUdność: liczba mężczyzn}
+#' \item{gestosc}{Gęstość zaludnienia [os./km^2]}
+#' }
+#'
+#'
+#' @source Bank Danych Lokalnych GUS http://stat.gov.pl/bdl/app/strona.html?p_name=indeks
+NULL
