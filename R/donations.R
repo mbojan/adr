b648fd382bc82200b698f78b3b02f6aefdc5fd7a
Michal Bojanowski <michal2992@gmail.com>
2017-04-05 23:54:00 +0200

    Dane donations

---
+++ R/donations.R
@@ -0,0 +1,11 @@
+#' donations: wpłaty na cele charytatywne w Holandii w 1993
+#'
+#' Podzbiór z badania sondażowego dotyczącego "filantropii" w społeczeństwie holenderskim w roku 1993.
+#'
+#' @docType data
+#' @name donations
+#' @encoding UTF-8
+#' 
+#' @format Ramka danych zawierająca 2000 obserwacji i 17 zmiennych.
+#'
+NULL
